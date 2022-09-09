8291c1ab723dd22e8629c12d5b0f2217b3455b8c
Nicholas Fell <sneah9@gmail.com>
2022-09-09 09:47:48 -0400

    shuffled deck bool parameter added

--- Deck.py
+++ Deck.py
@@ -1,20 +1,30 @@
-class deck:
-    def __init__(self, name, suit, value):
-        self.name = name
-        self.suit = suit
-        self.value = value
+import random
+names= ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+club= '\u2663'; spade= '\u2660'; heart=  '\u2665'; diamond= '\u2666' #plain suit variables for unicode strings
+suits= [club, spade, heart, diamond]
 
-    def create_deck():
+class cards:
+    def __init__(self, names, suits):
+        self.name = names
+        self.suit = suits
+
+def create_deck(names, suits, shuffled=False):
     deck= []
-    for suit in ['\u2663', '\u2660', '\u2665', '\u2666']:# define variables for unicode strings as hearts...
-        for card in ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']:  
-            deck.append(card+suit)
+    for name in names:
+        for suit in suits:  
+            deck.append(name+suit)
+    if shuffled == True:
+        random.shuffle(deck)
     return deck
 
-    def deck_values():
+def deck_values():
     values= []
     for x in range(4):
         for value in range(2,15):
             values.append(value)
-    key = dict(zip(deck, values))
+    key = dict(zip(cards, values))
     return key
+
+deck1= create_deck(names, suits, shuffled=False);
+
+print (deck1);
